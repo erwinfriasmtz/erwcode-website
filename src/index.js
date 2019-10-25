50584f4e66a836332e3e391c00dc4fc4ed40ec52
Erwin Alan Frías Martínez <alan.frias@erwcode.com>
2019-10-24 23:25:38 -0600

    responsive hamburguer menu component
    + new file:   src/images/imagotipo-horizont-white.svg
    + modified:   src/index.js
    + new file:   src/js/menu.js
    + new file:   src/pug/includes/menu.pug
    + modified:   src/pug/template.pug
    + new file:   src/scss/components/_menu.scss
    + deleted:    src/scss/components/_modal.scss
    + modified:   src/scss/layout/l-container.scss
    + modified:   src/scss/main.scss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import scss from './scss/main.scss';
+/* ********** Fonts ********** */
+import ligthFont from './fonts/Averta_Light.woff';
+import regularFont from './fonts/Averta.woff';
+import boldFont from './fonts/Averta_Bold.woff';
+import blackFont from './fonts/Averta_Black.woff';
+
+
+/* ********** CSS ********** */
+import scss from './scss/main.scss'
+
+
+/* ********** Images ********** */
+// import favicon      from './images/favicon.png'
+import logoHorizont from './images/imagotipo-horizont-white.svg'
+
+
+/* ********** JavaScript ********** */
+import { activeMenu } from './js/menu'
+
+activeMenu()
